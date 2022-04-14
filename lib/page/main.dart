a1f88b7d4bcad493678e22db356c86a775e6d957
Faris Hasim <farishasimsyauqi@gmail.com>
2022-04-14 13:32:39 +0700

    init project

---
+++ lib/page/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Galeri Kopi"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            child: Row(
+              children: [
+
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
